c28855e6fc65f575fb35d301004e871fb727576a
Lê Trần Hoàng Sơn <hoangson24198@gmail.com>
2021-02-22 14:30:35 +0700

    local test

--- app/src/main/java/com/hoangson/baseandroid/core/Configuration.kt
+++ app/src/main/java/com/hoangson/baseandroid/core/Configuration.kt
@@ -3,7 +3,7 @@ package com.hoangson.baseandroid.core
 class Configuration {
 
     companion object {
-        const val REMOTE_ENDPOINT = "https://hanihussein.pythonanywhere.com/demo/api/v1/"
+        const val REMOTE_ENDPOINT = "https://115.78.1.222:24481"
         const val DATABASE_NAME = ""
         const val SHARED_PREF_NAME = ""
     }
